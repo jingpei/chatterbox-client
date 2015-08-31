ce9b3ec2507adf3b3d6bd07a244ec9be6cd756dc
jingpei <jingjpei@gmail.com>
2015-08-31 15:52:52 -0700

    implement escaped character check one

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,62 @@
 // YOUR CODE HERE:
+//https://api.parse.com/1/classes/chatterbox
+
+var getMessages = function(){
+  var messages = $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox', 
+    type: 'GET', 
+    contentType: 'application/json', 
+    success: function(data, textStatus, jqXHR){
+      //data is an array of objects that contain all message data (users, time, message, etc.)
+      messages = data.results;
+      return data.results; 
+    } 
+  }).done(function(data){
+    updateMessages(data.results);
+    return data.results;
+  });
+  //messages = Object.create(messages)
+  // return messages;
+  //return messages.responseJSON.results; //WHY YOU NO WORK. 
+}
+
+var updateMessages = function(chats){
+  var $messageDisplay = $(".message-display"); 
+  for(var i = 0; i < chats.length; i++){
+
+    $messageDiv = $('<div></div>'); 
+    $messageDiv.addClass(chats[i].username);
+    $messageDiv.html('message: '+chats[i].text);
+    $messageDisplay.append($messageDiv);
+  }
+}
+
+var escapeCheck = function(text){
+  var newName ="";
+  for(var j = 0; j < text.length; j++){
+    if(text[j] === "<" ||
+      text[j] === ">" ||
+      text[j] === "&" ||
+      text[j] === "\"" ||
+      text[j] === "\'" ||
+      text[j] === "," ||
+      text[j] === "!" ||
+      text[j] === "@" ||
+      text[j] === "$" ||
+      text[j] === "%" ||
+      text[j] === "(" ||
+      text[j] === ")" ||
+      text[j] === "=" ||
+      text[j] === "+" ||
+      text[j] === "{" ||
+      text[j] === "}" ||
+      text[j] === "[" ||
+      text[j] === "]" 
+      ){
+      //do nothing 
+    }else{
+      newName+=text[j];
+    }
+  }
+  return newName;
+}
